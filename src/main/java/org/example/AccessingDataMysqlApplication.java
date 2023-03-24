0e54c05c01fc58374719b6b8e6ad41c69f574ced
Egor <eg.komarov14@gmail.com>
2023-03-24 15:26:02 +0100

    First add

---
+++ src/main/java/org/example/AccessingDataMysqlApplication.java
@@ -0,0 +1,10 @@
+package org.example.simpleWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@SpringBootApplication
+//public class AccessingDataMysqlApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(AccessingDataMysqlApplication.class, args);
+//    }
+//}
---
+++ src/main/java/org/example/simpleWeb/User.java
@@ -0,0 +1,2 @@
+package org.example.simpleWeb;public class User {
+}
